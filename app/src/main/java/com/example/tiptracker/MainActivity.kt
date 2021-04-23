2a530a3bda231120a68e377e5f999c390c2faf4d
Austin <safkoa17@gmail.com>
2021-04-22 23:52:30 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/tiptracker/MainActivity.kt
@@ -0,0 +1,292 @@
+package com.example.tiptracker
+
+import android.Manifest
+import android.app.Activity
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.textfield.TextInputEditText
+import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+/**
+Creates the main activity which will access daily data and display calories for the day.
+From here you can view the items you have previusly logged or add more items to the log
+ */
+class MainActivity : AppCompatActivity() {
+
+    val myCalendar = Calendar.getInstance()
+
+    override fun onResume() {
+        super.onResume()
+        loadFile()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        verifyStoragePermissions(this)
+        loadFile()
+
+        val viewLogButton: Button = findViewById(R.id.viewLog)
+        viewLogButton.setOnClickListener(View.OnClickListener {
+            viewLogButtonClick();
+        })
+
+        val addItemButton: Button = findViewById(R.id.addItem)
+        addItemButton.setOnClickListener(View.OnClickListener {
+            saveFile();
+        })
+
+        val dateTest = findViewById<View>(R.id.dateText) as EditText
+        val sdf = SimpleDateFormat("MM/dd/yyyy")
+        dateTest.setText(sdf.format(Date()))
+
+        val dateListener = OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            myCalendar[Calendar.YEAR] = year
+            myCalendar[Calendar.MONTH] = monthOfYear
+            myCalendar[Calendar.DAY_OF_MONTH] = dayOfMonth
+            updateLabel()
+            loadFile()
+        }
+
+        dateTest.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                DatePickerDialog(this@MainActivity, dateListener, myCalendar[Calendar.YEAR], myCalendar[Calendar.MONTH],
+                    myCalendar[Calendar.DAY_OF_MONTH]).show();
+            }
+        })
+
+    }
+
+    /**
+     * Loads the local file that we have written to from the device
+     *  and parses it for information.
+     */
+    private fun loadFile() {
+        val text = readFromFile(applicationContext)
+        val date = findViewById<TextView>(R.id.dateText);
+        parseText(text)
+    }
+
+    /**
+     * Logic for reading from a local file
+     */
+    private fun readFromFile(context: Context): String {
+        var ret = ""
+        try {
+            val inputStream: InputStream? = context.openFileInput("config.txt")
+            if (inputStream != null) {
+                val inputStreamReader =
+                    InputStreamReader(inputStream)
+                val bufferedReader =
+                    BufferedReader(inputStreamReader)
+                var receiveString: String? = ""
+                val stringBuilder = StringBuilder()
+                while (bufferedReader.readLine().also { receiveString = it } != null) {
+                    stringBuilder.append("\n").append(receiveString)
+                }
+                inputStream.close()
+                ret = stringBuilder.toString()
+            }
+            Log.d("Loaded file", ret)
+        } catch (e: FileNotFoundException) {
+            Log.e("login activity", "File not found: $e")
+        } catch (e: IOException) {
+            Log.e("login activity", "Can not read file: $e")
+        }
+        return ret
+    }
+
+    //Ensures the application has proper read/write permissions.
+    private val REQUEST_EXTERNAL_STORAGE = 1
+    private val PERMISSIONS_STORAGE = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    fun verifyStoragePermissions(activity: Activity?) {
+        // Check if we have write permission
+        val permission = ActivityCompat.checkSelfPermission(
+            activity!!,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                activity,
+                PERMISSIONS_STORAGE,
+                REQUEST_EXTERNAL_STORAGE
+            )
+        }
+        if (ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                112
+            )
+        }
+    }
+
+
+    /**
+     * Opens the ViewLog view when the button is pressed.
+     */
+    private fun viewLogButtonClick(){
+        val intent = Intent(this, ViewLog::class.java).apply {
+        }
+        val date = findViewById<TextView>(R.id.dateText);
+        intent.putExtra("date", date.text.toString())
+        startActivity(intent)
+    }
+
+    /**
+     * Parses the text from the local file to find
+     * only information for the selected date.
+     * @param fileText - String data from local file
+     */
+    private fun parseText(fileText: String) {
+
+        val date = findViewById<TextView>(R.id.dateText);
+        val dateString = date.text.toString()
+
+        val regex = Regex("DATE:"+dateString+":(.*?):END:")
+        val matches = regex.findAll(fileText)
+        val itemsInputted = matches.map { it.groupValues[0] }.joinToString()
+        //setValues(itemsInputted)
+
+        val typeSpinner: Spinner = findViewById(R.id.typeSpinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.typeArray,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_list_item_checked)
+            typeSpinner.adapter = adapter
+        }
+    }
+
+    /**
+     * Gets sum of a given string
+     */
+    fun findSum(str: String): Int {
+        var temp = "0"
+        var sum = 0
+
+        // read each character in input string
+        for (i in 0 until str.length) {
+            val ch = str[i]
+
+            if (Character.isDigit(ch)) temp += ch else {
+                sum += temp.toInt()
+                temp = "0"
+            }
+        }
+        return sum + temp.toInt()
+    }
+
+
+
+    private fun updateLabel() {
+        val dateText = findViewById<View>(R.id.dateText) as EditText
+        val myFormat = "MM/dd/yyyy" //In which you need put here
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        dateText.setText(sdf.format(myCalendar.getTime()))
+    }
+
+
+    fun saveFile() {
+        val date = findViewById<TextView>(R.id.dateText);
+        val dateValue: String = date.text.toString()
+        val address: EditText = findViewById(R.id.addressInput);
+        val deliveryCharge: EditText = findViewById(R.id.ratePerDeliveryInput);
+        val typeSpinner: Spinner = findViewById(R.id.typeSpinner);
+        val orderType = typeSpinner.selectedItem.toString()
+        val orderPrice: EditText = findViewById(R.id.priceInput);
+        val tipAmount: EditText = findViewById(R.id.tipInput);
+
+        var text = ""
+
+        if(orderType == "Cash"){
+            text = "DATE:" + dateValue + ":ADDRESS:" + address.text  + ":OTYPEM:" + orderType + ":OPRICEM:" + orderPrice.text +
+                    ":TIPM:" + tipAmount.text + ":CHARGE:" +
+                    deliveryCharge.text + ":STOP:" + ":END:"
+        } else if(orderType == "Credit") {
+            text = "DATE:" + dateValue + ":ADDRESS:" + address.text  + ":OTYPEC:" + orderType + ":OPRICEC:" + orderPrice.text +
+                    ":TIPC:" + tipAmount.text + ":CHARGE:" +
+                    deliveryCharge.text + ":STOP:" + ":END:"
+        }
+
+        if(dateValue == "" || address.text.toString() == "" || deliveryCharge.text.toString() == "" || orderPrice.text.toString() == "" ||
+                tipAmount.text.toString() == "") {
+            fillToastMsg("Fill out all fields");
+
+        }
+        else{
+            savedToastMsg("Added Item to Log");
+            writeToFile(text, applicationContext)
+        }
+
+    }
+
+    /**
+     * Logic for writing to a file.
+     */
+    private fun writeToFile(
+        data: String,
+        context: Context
+    ) {
+        Log.d("writeToFile", data)
+        try {
+            val outputStreamWriter = OutputStreamWriter(
+                context.openFileOutput(
+                    "config.txt",
+                    Context.MODE_APPEND
+                    //Context.MODE_PRIVATE
+                )
+            )
+            outputStreamWriter.write(data)
+            outputStreamWriter.close()
+            Log.d("Saved file", data)
+        } catch (e: IOException) {
+            Log.e("Exception", "File write failed: $e")
+        }
+    }
+
+    private fun savedToastMsg(msg: String?) {
+        val savedtoast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        savedtoast.show()
+    }
+
+    private fun fillToastMsg(msg: String?) {
+        val filltoast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        filltoast.show()
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/tiptracker/ViewLog.kt
@@ -0,0 +1,180 @@
+package com.example.tiptracker
+
+import android.content.Context
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.*
+
+
+/**
+ * Creates the ViewLog view which will show any items logged for the selected date.
+ */
+class ViewLog : AppCompatActivity() {
+
+
+
+    /**
+     * Loads the data from the local file each time the view is opened
+     */
+    override fun onResume() {
+        super.onResume()
+        loadFile()
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_log)
+        loadFile();
+
+        val values = findViewById<TextView>(R.id.values)
+
+
+    }
+
+    private fun loadFile() {
+        val text = readFromFile(applicationContext)
+        parseText(text)
+    }
+
+    private fun readFromFile(context: Context): String {
+        var ret = ""
+        try {
+            val inputStream: InputStream? = context.openFileInput("config.txt")
+            if (inputStream != null) {
+                val inputStreamReader =
+                        InputStreamReader(inputStream)
+                val bufferedReader =
+                        BufferedReader(inputStreamReader)
+                var receiveString: String? = ""
+                val stringBuilder = StringBuilder()
+                while (bufferedReader.readLine().also { receiveString = it } != null) {
+                    stringBuilder.append("\n").append(receiveString)
+                }
+                inputStream.close()
+                ret = stringBuilder.toString()
+            }
+            Log.d("Loaded file", ret)
+        } catch (e: FileNotFoundException) {
+            Log.e("login activity", "File not found: $e")
+        } catch (e: IOException) {
+            Log.e("login activity", "Can not read file: $e")
+        }
+        return ret
+    }
+
+    /**
+     * Parses the data from the local files for the given date
+     * @param fileText - File from local storage containing all item data
+     */
+    private fun parseText(fileText: String) {
+
+        val dateValue = intent.getStringExtra("date")
+        val regex = Regex("DATE:"+dateValue+":(.*?):END:")
+        val matches = regex.findAll(fileText)
+        val itemsInputted = matches.map { it.groupValues[0] }.joinToString()
+        setValues(itemsInputted)
+    }
+
+    /**
+     * Displays each item for the given meal for the selected date
+     * @param fileText - Text pared from local storage containing
+     * only information for the given data.
+     */
+    private fun setValues(fileText: String) {
+        val values = findViewById<TextView>(R.id.values)
+        val cashValue = findViewById<TextView>(R.id.cashTipsAmount)
+        val creditValue = findViewById<TextView>(R.id.cardTipsAmount)
+        val totalMade = findViewById<TextView>(R.id.totalMadeAmount)
+        val numDeliveries = findViewById<TextView>(R.id.numberDeliveries)
+        val oweStore = findViewById<TextView>(R.id.oweStoreAmount)
+        val totalCashOrders = findViewById<TextView>(R.id.cashOrderTotal)
+
+        val cashTip = Regex(":TIPM:(.*?):CHARGE:")
+        val cashTipMatches = cashTip.findAll(fileText)
+        var cashTipsInput = cashTipMatches.map { it.groupValues[1] }.joinToString().replace(", ", "+")
+        var cashTipsDouble = findSumFloat(cashTipsInput)
+        //cashTipsInput = findSumFloat(cashTipsInput).toString()
+        cashValue.setText("$" + cashTipsDouble.toString())
+
+        val creditTip = Regex(":TIPC:(.*?):CHARGE:")
+        val creditTipMatches = creditTip.findAll(fileText)
+        var creditTipsInput = creditTipMatches.map { it.groupValues[1] }.joinToString().replace(", ", "+")
+        var creditTipsDouble = findSumFloat(creditTipsInput)
+        //creditTipsInput = findSumFloat(creditTipsInput).toString()
+        creditValue.setText("$" + creditTipsDouble.toString())
+
+
+        val numDels = Regex(":CHARGE:(.*?):STOP:")
+        val numDelsMatches = numDels.findAll(fileText)
+        numDeliveries.setText((numDelsMatches.count()).toString())
+
+        val numDelsCharges = Regex(":CHARGE:(.*?):STOP:")
+        val numDelsChargesMatches = numDelsCharges.findAll(fileText)
+        var numDelsInput = numDelsChargesMatches.map { it.groupValues[1] }.joinToString().replace(", ", "+")
+        val numDelChargesDouble = findSumFloat(numDelsInput)
+
+        val totalCashOrder = Regex(":OPRICEM:(.*?):TIPM:")
+        val totalCashOrderMatches = totalCashOrder.findAll(fileText)
+        var totalCashOrderInput = totalCashOrderMatches.map { it.groupValues[1] }.joinToString().replace(", ", "+")
+        val totalCashOrderDouble = findSumFloat(totalCashOrderInput)
+        totalCashOrders.setText("$" + totalCashOrderDouble.toString())
+
+        val totalCreditTips = Regex(":TIPC:(.*?):CHARGE:")
+        val totalCreditTipsMatches = totalCreditTips.findAll(fileText)
+        var totalCreditTipsInput = totalCreditTipsMatches.map { it.groupValues[1] }.joinToString().replace(", ", "+")
+        val totalCreditTipsDouble = findSumFloat(totalCreditTipsInput)
+
+        var totalMadeInput = (cashTipsDouble + creditTipsDouble + numDelChargesDouble).toString()
+        totalMade.setText("$" + totalMadeInput)
+
+        oweStore.setText("$" + (totalCashOrderDouble - totalCreditTipsDouble - numDelChargesDouble).toString())
+
+
+        val valuesRegex = Regex("ADDRESS:(.*?):END:")
+        val valuesMatches = valuesRegex.findAll(fileText)
+        var valuesInput = valuesMatches.map { it.groupValues[1] }.joinToString().replace(",", "").replace(":OTYPEM:", " - ").replace(":OTYPEC:", " - ").replace(":OPRICEM:", " - Order:$").replace(":OPRICEC:", " - Order:$").replace(":TIPM:", " - Tip:$").replace(":TIPC:", " - Tip:$").replace(":CHARGE:", " - Charge:$").replace(":STOP:","\n");
+        values.setText(" " + valuesInput)
+
+    }
+
+
+    fun findSum(str: String): Int {
+        var temp = "0"
+        var sum = 0
+
+        // read each character in input string
+        for (i in 0 until str.length) {
+            val ch = str[i]
+
+            if (Character.isDigit(ch)) temp += ch else {
+                sum += temp.toInt()
+                temp = "0"
+            }
+        }
+        return sum + temp.toInt()
+    }
+
+    fun findSumFloat(str: String): Double {
+        var temp = "0"
+        var sum = 0.0
+
+        // read each character in input string
+        for (i in 0 until str.length) {
+            val ch = str[i]
+
+            if (Character.isDigit(ch) || ch == '.') temp += ch else {
+                sum += temp.toDouble()
+                temp = "0"
+            }
+        }
+        return sum + temp.toDouble()
+    }
+
+
+
+
+}
